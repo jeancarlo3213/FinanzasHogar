f64744f308fc327458a334d58f430a2db53f3d28
jeancarlo3213 <149128952+jeancarlo3213@users.noreply.github.com>
2024-08-20 19:44:11 -0600

    Pasivo terminado

---
+++ Models/TipoPasivo.cs
@@ -0,0 +1,14 @@
+﻿namespace FinanzasHogar.Models
+{
+    public static class TipoPasivo
+    {
+        public static List<string> Tipos = new List<string>
+        {
+            "Deuda",
+            "Hipoteca",
+            "Préstamo",
+            "Tarjeta de Crédito",
+            "Otros"
+        };
+    }
+}
